7783a23a0b03f3ad7c8b20330ed05e4af0784203
dirkdirk <dirkdirk@users.noreply.github.com>
2017-03-21 03:25:40 -0700

    Refactor code after fixing Simditors value bug.

--- app/controllers/jots/jot.js
+++ app/controllers/jots/jot.js
@@ -1,20 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  modelEditor: Ember.computed.alias('model.editor'),
-
-  // modelText: Ember.computed.alias('model.text'),
-  // text: function() {
-  //   let x = this.get('modelText');
-  //   return x;
-  // }.observes('modelId'),
 
   modelId: Ember.computed.alias('model.id'),
   addSelectedClassToLink: function() {
     Ember.$('.menu-jot-link').removeClass('jot-link-selected');
     Ember.$('#jot-link-' + this.get('modelId')).addClass('jot-link-selected');
-    // Ember.$('.simditor').parent().remove();
-    // Ember.rerender();
   }.observes('modelId'),
 
   updateStatusIconColorObserver: function() {
@@ -45,7 +36,6 @@ export default Ember.Controller.extend({
     saveJotSlow() {
       console.log('--> saveJotTextSlow() firing');
       Ember.run.debounce(this, this.saveDirtyModel, 10000);
-      // this.saveDirtyModel();
     },
     destroyJot(model) {
       console.log('--> destroyJot() firing ...');
@@ -65,36 +55,6 @@ export default Ember.Controller.extend({
                'table', 'link', 'image',
                'hr', 'indent', 'outdent',
                'alignment' ],
-  },
-  mediumOptions: {
-    // https://github.com/yabwe/medium-editor
-    // Changing themes doesn't seem to work.
-    // theme: 'bootstrap',
-    // theme: 'default',
-    // theme: 'flat',
-    // theme: 'mani',
-    // theme: 'roman',
-    toolbar: {
-      buttons: ['h2', 'h4', 'bold', 'italic', 'underline', 'strikethrough',
-                'pre', 'quote',
-                'unorderedlist', 'orderedlist',
-                'outdent', 'indent',
-                'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull',
-                'anchor', 'removeFormat'],
-      static: true,
-      sticky:  true,
-      // stickyTopOffset: -60,
-      updateOnEmptySelection: true,
-      align: 'center',
-
-
-    },
-    // buttonLables: 'fontawesome',  // May have to edit bower_components/medium-editor/dist/js/medium-editor.js
-    autoLink: true,
-    imageDragging: false,
-    extensions: {
-      imageDragging: {},
-    }
   },
 
 });
