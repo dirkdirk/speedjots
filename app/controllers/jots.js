2c94956ea2f62a8ef57ef6546c33c1a9df30d5e7
dirkdirk <dirkdirk@users.noreply.github.com>
2017-03-23 14:33:41 -0700

    Add drag n drop of Jots.

--- app/controllers/jots.js
+++ app/controllers/jots.js
@@ -2,10 +2,12 @@ import Ember from 'ember';
 import groupBy from 'ember-group-by';
 
 export default Ember.Controller.extend({
+  panelActions: Ember.inject.service(),
+
   sortModelBy: ['title'],
   sortedModel: Ember.computed.sort('model', 'sortModelBy'),
+  jotsByGroup: groupBy('sortedModel', 'group'),
   sortGroupsBy: ['value'],
-  jotsByGroup: groupBy('model', 'group'),
   sortedJotsByGroup: Ember.computed.sort('jotsByGroup', 'sortGroupsBy'),
 
   saveNewJot() {
@@ -24,6 +26,36 @@ export default Ember.Controller.extend({
   actions: {
     newJot() {
       Ember.run.debounce(this, this.saveNewJot, 300);
-    }
+    },
+    addJotToGroup(jot, ops) {
+      let tagetTitle = ops.target.groupTitle;
+      let toGroup = tagetTitle ? tagetTitle : getNewGroupName(this.get('model'));
+      jot.set('group', toGroup);
+      jot.save();
+    },
+    openAllGroups()  { this.get('panelActions').openAll('allGroups'); },
+    closeAllGroups() { this.get('panelActions').closeAll('allGroups'); },
   }
+
 });
+
+
+var getNewGroupName = function(model) {
+  let i = 1;
+  let groups = model.mapBy('group').uniq();
+  let toGroup = 'Speed Group';
+  while(groups.includes(toGroup) && i < 99) {
+    if(i === 1) {
+      toGroup = toGroup + ' ' + i;
+    } else {
+      let lastSpace = toGroup.lastIndexOf(' ');
+      toGroup = toGroup.slice(0, lastSpace + 1) + i;
+    }
+    if(groups.includes(toGroup)) {
+      groups.push(toGroup);
+    } else {
+      return toGroup;
+    }
+    i += 1;
+  }
+};
