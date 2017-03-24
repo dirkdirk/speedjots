20b80a6e21edcb58280b847c162951d87ee2580b
dirkdirk <dirkdirk@users.noreply.github.com>
2017-03-23 17:49:09 -0700

    Refine drag and drop.

--- app/controllers/jots.js
+++ app/controllers/jots.js
@@ -26,36 +26,48 @@ export default Ember.Controller.extend({
   actions: {
     newJot() {
       Ember.run.debounce(this, this.saveNewJot, 300);
+      this.send('openPanel', 'Not Grouped');
     },
     addJotToGroup(jot, ops) {
       let tagetTitle = ops.target.groupTitle;
       let toGroup = tagetTitle ? tagetTitle : getNewGroupName(this.get('model'));
       jot.set('group', toGroup);
       jot.save();
+      this.send('openPanel', toGroup);
+      clearAllJotHighlighting();
     },
-    openAllGroups()  { this.get('panelActions').openAll('allGroups'); },
-    closeAllGroups() { this.get('panelActions').closeAll('allGroups'); },
+    openAllGroups()      { this.get('panelActions').openAll('allGroups'); },
+    closeAllGroups()     { this.get('panelActions').closeAll('allGroups'); },
+    openPanel(panelName) { this.get('panelActions').open(panelName); },
+    // closePanel(panelName)  { this.get('panelActions').close(panelName); },
+    // togglePanel(panelName) { this.get('panelActions').toggle(panelName); },
   }
 
 });
+
+var clearAllJotHighlighting = function() {
+  Ember.$('.cp-Panel-toggle').blur(function() { console.log('moooot');});
+  // Ember.$('.menu-jot-link').blur(function() { console.log('moooot');});
+  // Ember.$('.menu-jot-link').first('div').blur(function() { console.log('blurrrr');});
+};
 
 
 var getNewGroupName = function(model) {
   let i = 1;
   let groups = model.mapBy('group').uniq();
   let toGroup = 'Speed Group';
-  while(groups.includes(toGroup) && i < 99) {
+  do {
+    if(groups.includes(toGroup)) {
+      groups.push(toGroup);
+    } else {
+      return toGroup;
+    }
     if(i === 1) {
       toGroup = toGroup + ' ' + i;
     } else {
       let lastSpace = toGroup.lastIndexOf(' ');
       toGroup = toGroup.slice(0, lastSpace + 1) + i;
     }
-    if(groups.includes(toGroup)) {
-      groups.push(toGroup);
-    } else {
-      return toGroup;
-    }
-    i += 1;
-  }
+    i++;
+  } while (!groups.includes(toGroup) && i < 99);
 };
